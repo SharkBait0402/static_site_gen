71b200401f3615bad29932ca2853adbd0e354d9f
Casen Nagy <casen.nagy@gmail.com>
2025-10-02 22:43:43 -0500

    added block type enum

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,4 @@
+
 def markdown_to_blocks(text):
     blocks = []
     tmp = text.split("\n\n")
@@ -7,3 +8,14 @@ def markdown_to_blocks(text):
             continue
         blocks.append(block.strip())
     return blocks
+
+class BlockType(Enum):
+    P = "paragragh"
+    H = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UL = "unordered list"
+    OL = "ordered list"
+
+def block_to_block_type(block):
+    pass
