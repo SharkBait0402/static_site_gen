b25226f23e48fe77cf1d806f4cf6dcd66f67d583
Casen Nagy <casen.nagy@gmail.com>
2025-10-11 19:06:43 -0500

    did basically nothing tbh... will continue later, i hope

--- src/blocks.py
+++ src/blocks.py
@@ -48,3 +48,12 @@ def block_to_block_type(block):
     if is_ol:
         return BlockType.OL
     return BlockType.P
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    for block in blocks:
+        type = block_to_block_type(block)
+
+        
