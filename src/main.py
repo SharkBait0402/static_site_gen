0b7c303347aac025894e3e0c9fc540a44755f938
Casen Nagy <casen.nagy@gmail.com>
2025-10-25 11:28:28 -0500

    brb gotta restart

--- src/main.py
+++ src/main.py
@@ -4,9 +4,15 @@ import shutil
 
 def copy_from_public():
     files = os.listdir("static")
+    path = "static"
 
     shutil.rmtree("public")
     os.mkdir("public")
+
+    for file in files:
+        path += f'/{file}'
+        print(os.path.isfile(path), '\n')
+        path = 'static'
 
 def main():
     copy_from_public()
